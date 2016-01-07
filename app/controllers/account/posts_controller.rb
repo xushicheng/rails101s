9aaaf34030a35713aae4bb7c3fb46dd56f659d0a
xushicheng <xushicheng@xjgreat.com>
2016-01-07 11:11:35 +0800

    返回后删除

--- app/controllers/account/posts_controller.rb
+++
@@ -1,8 +0,0 @@
-class Account::PostsController < ApplicationController
-
-  before_action :authenticate_user!
-
-  def index
-    @posts = current_user.posts.order.recent
-  end
-end
--- app/helpers/posts_helper.rb
+++
@@ -1,5 +0,0 @@
-module PostsHelper
-  def render_post_content(post)
-    truncate(simple_format(post.content), lenth: 100)
-  end
-end
